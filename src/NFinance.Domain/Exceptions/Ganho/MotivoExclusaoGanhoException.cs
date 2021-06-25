06593b67da37aecdcdc54d04aa133c21a9b5f443
João Gabriel Kasprik Arruda <jgkasprik@hotmail.com>
2021-06-25 16:32:00 -0300

    Excluir motivo exclusao

---
+++ src/NFinance.Domain/Exceptions/Ganho/MotivoExclusaoGanhoException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace NFinance.Domain.Exceptions.Ganho
+{
+    [Serializable]
+    public class MotivoExclusaoGanhoException : DomainException
+    {
+        public override string ErrorCode => "ERRO_MOTIVO_EXCLUSAO_GANHO";
+        private const string DefaultMessage = "O motivo inserido e invalido";
+
+        public MotivoExclusaoGanhoException() : this(DefaultMessage)
+        {
+        }
+
+        public MotivoExclusaoGanhoException(string message) : base(message)
+        {
+        }
+
+        protected MotivoExclusaoGanhoException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
